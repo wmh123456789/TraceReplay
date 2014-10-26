a6584abff3c9c421175c5645e134849a43975d45
Minghui Wang <wmh123456789@outlook.com>
2014-10-26 18:30:54 +0800

    Objects connection OK

--- ReplayGUI.py
+++ ReplayGUI.py
@@ -14,13 +14,18 @@ class DisplayFrame(object):
 
 class Controler(DisplayFrame):
 	"""docstring for Controler"""
-	def __init__(self,size,title=''):
+	def __init__(self,size,title='',Caller=None):
 		super(Controler, self).__init__(size,title)
 		# self.label = Label(self.top,text='This is Controler',font = 'Helvetica -12 bold')
 		# self.label.pack(fill=Y, expand=0)
 		self.Btn_quit = Button(self.top,text='QUIT',command=self.top.quit,activeforeground='white',
-		activebackground='red')
+								activebackground='red')
 		self.Btn_quit.pack(side=BOTTOM)
+		self.ProgBar = Scale(self.top,from_=0,to=18000,orient=HORIZONTAL,command=Caller.OnProgBarMove)
+		self.ProgBar.pack(fill=X,side=BOTTOM)
+
+	
+
 
 class ShowPath(DisplayFrame):
 	"""docstring for ShowPath"""
@@ -40,19 +45,21 @@ class GUITop(object):
 	"""docstring for GUITop"""
 	def __init__(self,trace):
 		super(GUITop, self).__init__()
+		self.trace = trace
 		# self.arg = arg
-		Con = Controler('800x600','Control Panel')		
-		Show = ShowPath('400x300','Location Trace',trace)
+		self.Con = Controler('800x600','Control Panel',Caller=self)		
+		self.Show = ShowPath('400x300','Location Trace',trace)
 
 
-
-
-
-
-		Con.top.mainloop()
-		Show.top.mainloop()
+		self.Con.top.mainloop()
+		self.Show.top.mainloop()
 		
-
+	def OnProgBarMove(self,ev=None):
+		# print self.Con.ProgBar.get()
+		extent = self.Con.ProgBar.get()*2/100
+		self.Show.C.coords(self.Show.rec,(50+extent, 25, 55+extent, 30))
+		return self.Con.ProgBar.get()
+		pass
 
 
 def main():
