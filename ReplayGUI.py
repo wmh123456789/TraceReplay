6d884536ea5d5f6aaa4c30b4e819817921e1dfd4
wmh123456789 <wmh123456789@outlook.com>
2014-10-30 10:28:50 +0800

    Update configure UI

--- ReplayGUI.py
+++ ReplayGUI.py
@@ -19,6 +19,10 @@ class DisplayFrame(object):
 		self.TraceList.append(trace)
 		print 'Trace',trace.tag,'is added'
 
+
+	def ClearAllTrace(self):
+		#ToDo: Renew all traces 
+		pass
 	
 class Controler(DisplayFrame):
 	"""docstring for Controler"""
@@ -28,24 +32,40 @@ class Controler(DisplayFrame):
 		# Frames
 		self.f_text = Frame(self.top)
 		self.f_text.pack(side=TOP,expand=1,fill=X)
+		self.f_config = Frame(self.top)
+		self.f_config.pack(side=TOP,expand=1,fill=X)
 		self.f_bar = Frame(self.top)
 		self.f_bar.pack(side=TOP,expand=1,fill=X)
 		self.f_bottom = Frame(self.top)
 		self.f_bottom.pack(side=BOTTOM,expand=1,fill=X)
 
-		# Parameters and configure
+		# Canvas configure(Shift, Scale)
 		self.label1 = Label(self.f_text,text='SftX',font = 'Helvetica -12 bold')
 		self.label1.pack(side=LEFT)
-		self.text1 = Text(self.f_text,height=1,width=8)
+		self.text1 = Text(self.f_text,height=1,width=9)
 		self.text1.pack(side=LEFT)
 		self.label2 = Label(self.f_text,text='SftY',font = 'Helvetica -12 bold')
 		self.label2.pack(side=LEFT)
-		self.text2 = Text(self.f_text,height=1,width=8)
+		self.text2 = Text(self.f_text,height=1,width=9)
 		self.text2.pack(side=LEFT)
 		self.label3 = Label(self.f_text,text='Scale',font = 'Helvetica -12 bold')
 		self.label3.pack(side=LEFT)
-		self.text3 = Text(self.f_text,height=1,width=8)
+		self.text3 = Text(self.f_text,height=1,width=9)
 		self.text3.pack(side=LEFT)
+		self.Btn_Confirm = Button(self.f_text,text='Confirm',command = Caller.OnConfirm)
+		self.Btn_Confirm.pack(side=RIGHT)
+
+		# Trace option
+		self.lb_tracepath = Label(self.f_config,text='TracePath',font = 'Helvetica -12 bold')
+		self.lb_tracepath.pack(side=LEFT)
+		self.tx_tracepath = Text(self.f_config,height=1,width=22)
+		self.tx_tracepath.pack(side=LEFT)
+		self.lb_opt = Label(self.f_config,text='TraceMode',font = 'Helvetica -12 bold')
+		self.lb_opt.pack(side=LEFT)
+		self.tx_opt = Text(self.f_config,height=1,width=2)
+		self.tx_opt.pack(side=LEFT)
+		self.Btn_reload = Button(self.f_config,text='Reload',command = Caller.OnReloadTrace)
+		self.Btn_reload.pack(side=RIGHT)
 
 		# Prograss Bar
 		self.ProgBar = Scale(self.f_bar,from_=0,to=100,orient=HORIZONTAL,command=Caller.OnProgBarMove)
@@ -60,8 +80,7 @@ class Controler(DisplayFrame):
 								activebackground='red')
 		self.Btn_quit.pack(side=RIGHT)
 
-		self.Btn_Confirm = Button(self.f_text,text='Confirm',command = Caller.OnConfirm)
-		self.Btn_Confirm.pack(side=RIGHT)
+		
 
 
 		# self.TracePath1 = Text(self.top)
@@ -115,16 +134,17 @@ class ShowPath(DisplayFrame):
 
 class GUITop(object):
 	"""docstring for GUITop"""
-	def __init__(self,LTrace,RTrace):
+	def __init__(self,LTrace,RTrace,TracePath=''):
 		super(GUITop, self).__init__()
 		self.LTrace = LTrace
 		self.RTrace = RTrace
+		self.TracePath = TracePath
 		self.FitTwoTraces(self.LTrace,self.RTrace)
 		# print str(self.LTrace), '\n\n'
 		# print str(self.RTrace)
 		# self.arg = arg
 		self.Show = ShowPath('1600x1200','Location Trace','TongFangD19.jpg')
-		self.Con = Controler('400x100','Control Panel',Caller=self)		
+		self.Con = Controler('450x150','Control Panel',Caller=self)		
 		
 		self.Con.AddTrace(self.LTrace)
 		self.Con.AddTrace(self.RTrace)
@@ -155,7 +175,7 @@ class GUITop(object):
 			print 'Error: Not enough traces to check length...'
 			return -1
 
-	def FitTwoTraces(slef,TA,TB):
+	def FitTwoTraces(self,TA,TB):
 		starttime = max([TA.starttime,TB.starttime])
 		endtime = min([TA.endtime,TB.endtime])
 		# TODO: clip the trace
@@ -173,6 +193,27 @@ class GUITop(object):
 		TB.RefreshTrace()
 
 		pass
+
+	def OnReloadTrace(self,ev=None):
+		xml = XMLFile(self.TracePath)
+		self.LTrace = LocTrace([xml.timestamplist_L,xml.Xlist_L,xml.Ylist_L],tag='L')
+		self.RTrace = LocTrace([xml.timestamplist_R,xml.Xlist_R,xml.Ylist_R],tag='R')
+		self.LTrace.LinearInterpolation()
+		self.RTrace.LinearInterpolation()
+		self.FitTwoTraces(self.LTrace,self.RTrace)
+		self.Con.ClearAllTrace()
+		self.Show.ClearAllTrace()
+		self.Con.AddTrace(self.LTrace)
+		self.Con.AddTrace(self.RTrace)
+		self.Show.AddTrace(self.LTrace)
+		self.Show.AddTrace(self.RTrace)
+		self.TotalErr = 0.0
+		self.TotalPts = 0
+		self.AvgErr = 0.0
+
+		pass
+
+
 
 	def OnConfirm(self,ev=None):
 		self.Show.SftX = float(self.Con.text1.get(1.0,END))
@@ -194,16 +235,18 @@ class GUITop(object):
 			self.Show.C.coords(rec,(PosX(time), PosY(time), W+PosX(time), H+PosY(time)))
 
 			# Keep a trace on map 
-			if '1' in self.Con.text3.get(1.0,END):     # Points mode
+			if '1' in self.Con.tx_opt.get(1.0,END):     # Points mode
 				self.Show.C.create_rectangle(PosX(time), PosY(time), W+PosX(time), H+PosY(time), 
 							fill=self.Show.recStyle[trace.tag][0], 
 							outline=self.Show.recStyle[trace.tag][1])
-			if '2' in self.Con.text3.get(1.0,END):    # Lines mode
+			if '2' in self.Con.tx_opt.get(1.0,END):    # Lines mode
 				if time > trace.starttime:
 					#ToDo: Draw a line 
-					self.Show.C.create_line(PosX(time-1),PosY(time-1),PosX(time),PosY(time))
+					self.Show.C.create_line(PosX(time-1),PosY(time-1),PosX(time),PosY(time),
+							fill=self.Show.recStyle[trace.tag][0],
+							width = 2.5)
 					pass
-			if '3' in  self.Con.text3.get(1.0,END):    # Tag on trace
+			if '3' in  self.Con.tx_opt.get(1.0,END):    # Tag on trace
 				#TODO: add a tag on the current point
 				pass
 
