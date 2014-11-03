cfe4af9a909c99bf580d820672556ed72ef1ec48
wmh123456789 <wmh123456789@outlook.com>
2014-11-03 17:39:47 +0800

    Try menu

--- Try.py
+++ Try.py
@@ -10,8 +10,8 @@
 # D = {1:'a',2:'b',3:'c'}
 # D.pop(2)
 
-A = lambda x,y:x+y
-print A(1,2)
+# A = lambda x,y:x+y
+# print A(1,2)
 
 
 # image = Image.open("pikachou.jpg")
@@ -74,3 +74,16 @@ print A(1,2)
 
 # root.mainloop()
 
+from Tkinter import *
+root = Tk()
+def hello():
+    print 'hello menu'
+menubar = Menu(root)
+
+filemenu = Menu(menubar,tearoff = 0)
+for item in ['Python','PHP','CPP','C','Java','JavaScript','VBScript']:
+    filemenu.add_command(label = item,command = hello)
+#将menubar的menu属性指定为filemenu，即filemenu为menubar的下拉菜单
+menubar.add_cascade(label = 'Language',menu = filemenu)
+root['menu'] = menubar
+root.mainloop()
